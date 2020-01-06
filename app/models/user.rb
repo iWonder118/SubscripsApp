cb51a8cd372446645df0f25e35ec3c5ff49a82c5
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-06 15:21:44 +0900

    Update-varidation&test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   has_secure_password validations: true
-  validates :name,                  presence: true
-  validates :email,                 presence: true, uniqueness: true
-  validates :password,              presence: true
-  validates :password_confirmation, presence: true
+
+  VALID_EMAIL_REGEX    = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  validates :name,                  presence: true, length: { in: 3..20 } 
+  validates :email,                 presence: true, format: { with: VALID_EMAIL_REGEX, message: "は@を含む形式にしてください" }, uniqueness: true,
+  validates :password,              presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字両方を含むパスワードを設定してください" }
+                                    ,length: { in: 8..128 }, confirmation: true
+  validates :password_confirmation, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字両方を含むパスワードを設定してください" }
+                                    ,length: { in: 8..128 }
 
   def self.new_remember_token
     SecureRandom.urlsafe_base64
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,96 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
+    it "全ての値が適切に入力されている場合、登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-    it "nameカラムが空の場合、登録できないこと" do
+    it "nameが空の場合、登録できないこと" do
       user = build(:user, name: "")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    it "emailカラムが空の場合、登録できないこと" do
+    it "nameが2文字以下のとき、登録できないこと" do
+      user = build(:user, name: "11")
+      user.valid?
+      expect(user.errors[:name]).to include("は3文字以上で入力してください")
+    end
+
+    it "nameが21文字以上のとき、登録できないこと" do
+      user = build(:user, name: "111111111111111111111")
+      user.valid?
+      expect(user.errors[:name]).to include("は20文字以内で入力してください")
+    end
+
+    it "emailが空の場合、登録できないこと" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
-    it "passwordカラムが空の場合、登録できないこと" do
-      user = build(:user, password: "")
+
+    it "emailの中間に@がない場合、登録できないこと" do
+      user = build(:user, email: "email")
+      user.valid?
+      expect(user.errors[:email]).to include("は@を含む形式にしてください")
+    end
+
+    it "emailが登録済みの場合、登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "passwordが空の場合、登録できないこと" do
+      user = build(:user, password: "", password_confirmation: "")
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
-    it "password_confirmationカラムが空の場合、登録できないこと" do
-      user = build(:user, password_confirmation: "")
+
+    it "passwordが7文字以下の場合、登録できないこと" do
+      user = build(:user, password: "12345qw", password_confirmation: "12345qw")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("を入力してください")
+      expect(user.errors[:password]).to include("8文字以上で入力してください", "は英字と数字両方を含むパスワードを設定してください")
+    end
+
+    it "passwordが129文字以上の場合、登録できないこと" do
+      o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+      password = (0...129).map { o[rand(o.length)] }.join
+      user = build(:user, password: password.to_s, password_confirmation: password.to_s)
+      user.valid?
+      expect(user.errors[:password]).to include("は128文字以内で入力してください")
+    end
+
+    it "passwordが数字のみの場合、登録できないこと" do
+      user = build(:user, password: "12345678", password_confirmation: "12345678")
+      user.valid?
+      expect(user.errors[:password]).to include("は英字と数字両方を含むパスワードを設定してください")
+    end
+
+    it "passwordが英字のみの場合、登録できないこと" do
+      user = build(:user, password: "abcdefgh", password_confirmation: "abcdefgh")
+      user.valid?
+      expect(user.errors[:password]).to include("は英字と数字両方を含むパスワードを設定してください")
+    end
+
+    it "passwordが数字と英字を含む8文字の場合、登録できること" do
+      user = build(:user, password: "1a2b3c4e", password_confirmation: "1a2b3c4e")
+      expect(user).to be_valid
+    end
+
+    it "passwordが数字と英字を含む128文字の場合、登録できること" do
+      o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+      password = (0...128).map { o[rand(o.length)] }.join
+      user = build(:user, password: password.to_s, password_confirmation: password.to_s)
+      expect(user).to be_valid
+    end
+
+    it "passwordと_confirmationが一致しない場合、登録できないこと" do
+      user = build(:user, password: "1a2b3c4e", password_confirmation: "1a2b3c3e")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
   end
