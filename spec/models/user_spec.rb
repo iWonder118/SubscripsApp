cb5030d61f114f0dba58d6010548f0c13ae0104d
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-06 14:23:33 +0900

    add-password-test #4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,16 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
+    it "passwordカラムが空の場合、登録できないこと" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+    it "password_confirmationカラムが空の場合、登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("を入力してください")
+    end
 
   end
 end
