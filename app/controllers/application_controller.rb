48616472dea4a0d645332b6a89564d2012061a26
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-06 17:01:27 +0900

    細かいところを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:user_remember_token] = remember_token
-    user.update!(remember_token: User.encrypt(remember_token))
+    user.update!(remember_token: User.encrypt(remember_token),password: session_params[:password],password_confirmation:session_params[:password])
     @current_user = user
   end
 
