4480e9bab219c8e2764e1c25cfee9a2e0edff23c
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-07 19:40:13 +0900

    Update-item-table #6

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_033843) do
+ActiveRecord::Schema.define(version: 2020_01_07_082902) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "link"
+    t.text "description"
+    t.string "price", null: false
+    t.boolean "private", default: false, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -27,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_01_05_033843) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "users"
 end
