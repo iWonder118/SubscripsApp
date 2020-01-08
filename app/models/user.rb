eeefc50a34ee7f0707a4ced851ca1869b57daa41
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-08 19:47:18 +0900

    Apply-Devise #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items, dependent: :destroy
+  
+  VALID_EMAIL_REGEX    = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  validates :name,                  presence: true, length: { in: 3..20 } 
+  validates :email,                 presence: true, format: { with: VALID_EMAIL_REGEX,    message: "は@を含む形式にしてください" }, uniqueness: true
+  validates :password,              presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字両方を含むパスワードを設定してください" },
+                                    length: { in: 8..72 }, confirmation: true
+  validates :password_confirmation, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字両方を含むパスワードを設定してください" },
+                                    length: { in: 8..72 }
 end
