218dee7cd9002a0ea4d57b8677b87863921b312f
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-10 15:01:23 +0900

    Create-item-create-action #6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:index]
 
   def index
-    @item = Item.new
-    @item.build_payment
     @items = Item.all
   end
 
   def create
-    item = params.require(:item).permit(:title)
-    Item.create(item)
+    @item = Item.create(item_params)
     redirect_to :root
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:title, :link, :color, :plan, :price, :private,
+                                payment_attributes:[:id, :period_long, :period_unit, :first_payment, :pay_method, :description]).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.new
+    @item.build_payment
   end
 
 end
