6b822b0b9851307566e86ae11acab273fb497286
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-13 14:15:12 +0900

    Add-item-validation #6

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   belongs_to :user
   
   accepts_nested_attributes_for :payment
+
+  validates :title,   presence: true, length: { in: 3..40 } 
+  validates :link,    presence: true
+  validates :color,   presence: true
+  validates :plan,    presence: true, length: { in: 3..40 } 
+  validates :price,   presence: true, numericality: true, length: { in: 100..1000000 } 
+  validates :private, acceptance: true
 end
