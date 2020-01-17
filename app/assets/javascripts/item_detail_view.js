e071d4aa249ee0f95481ef0d755148356c784713
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-17 18:18:19 +0900

    Create-show-all-off-button #12

--- app/assets/javascripts/item_detail_view.js
+++ app/assets/javascripts/item_detail_view.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load', function () {
       $(this).parent().css('display', 'none');
       $(this).parent().next(".content__body").css('display', 'flex');
       $(this).parent().nextAll('.show-off').css('display', 'block');
-    })
+    });
 
     //アイテムの詳細 非 表示処理
     $(document).on('click', '.show-off__button', function (e) {
@@ -15,6 +15,14 @@ $(document).on('turbolinks:load', function () {
       $(this).parent().css('display', 'none');
       $(this).parent().prev(".content__body").css('display', 'none');
       $(this).parent().prevAll('.show-on').css('display', 'block');
-    })
+    });
+
+    //アイテムの全詳細 非 表示処理
+    $(document).on('click', '#show_all_off', function (e) {
+      e.preventDefault();
+      $(".content__body").css('display', 'none');
+      $('.show-off').css('display', 'none');
+      $('.show-on').css('display', 'block');
+    });
   });
 });
