9d32cfe786cf420e3b95ba96630cf1d1061b8dad
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-28 22:58:32 +0900

    add-row-order-column #23

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_093414) do
+ActiveRecord::Schema.define(version: 2020_01_28_135504) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_093414) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row_order"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
