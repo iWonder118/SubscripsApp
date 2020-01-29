e4da937beb472b045fd0614a27c4a638b03cb8a2
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-29 11:52:02 +0900

    fix-puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-if ENV.fetch("RAILS_ENV") == "production"
+if ENV["RAILS_ENV"] == "production"
   workers 1 
 end
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
