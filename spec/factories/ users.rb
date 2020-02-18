2e85b16bedbd58a1926778a4b4470678a2a42079
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-02-18 14:30:28 +0900

    Fix-Faker-password

--- spec/factories/ users.rb
+++ spec/factories/ users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    pass = Faker::Internet.password(min_length: 8)
+    pass = Faker::Internet.password(min_length = 8, max_length = 16,)
 
     name                  {Faker::Name.name}
     email                 {Faker::Internet.email}
